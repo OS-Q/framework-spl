18538be4f67039040ecd45bab3df55fc224da7ab
qitas <qitas@qitas.cn>
2021-03-02 13:24:32 +0800

    fix

--- tests/GPIO_L1/src/main.c
+++ tests/GPIO_L1/src/main.c
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * @file    GPIO/IOToggle/main.c 
+  * @file    GPIO/IOToggle/main.c
   * @author  MCD Application Team
   * @version V1.2.1
   * @date    20-April-2015
@@ -16,8 +16,8 @@
   *
   *        http://www.st.com/software_license_agreement_liberty_v2
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
@@ -27,18 +27,14 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l1xx.h"
+#include "stm32l1xx_rcc.h"
+#include "stm32l1xx_gpio.h"
 
-#ifdef USE_STM32L152D_EVAL 
-  #include "stm32l152d_eval.h"
-  #define GPIO_PIN_X GPIO_Pin_3
-  #define GPIO_PIN_Y GPIO_Pin_7
-  #define BSRR_VAL 0x88
-#elif defined USE_STM32L152_EVAL 
-  #include "stm32l152_eval.h"
-  #define GPIO_PIN_X GPIO_Pin_0
-  #define GPIO_PIN_Y GPIO_Pin_1
-  #define BSRR_VAL 0x03
-#endif 
+
+#define GPIO_PIN_X GPIO_Pin_0
+#define GPIO_PIN_Y GPIO_Pin_1
+#define BSRR_VAL 0x03
+
 
 /** @addtogroup STM32L1xx_StdPeriph_Examples
   * @{
@@ -46,7 +42,7 @@
 
 /** @addtogroup IOToggle
   * @{
-  */ 
+  */
 
 /* Private typedef -----------------------------------------------------------*/
 GPIO_InitTypeDef        GPIO_InitStructure;
@@ -64,27 +60,27 @@ GPIO_InitTypeDef        GPIO_InitStructure;
   */
 int main(void)
 {
-  /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       file (startup_stm32l1xx_xx.s) before to branch to application main.
-       To reconfigure the default setting of SystemInit() function, refer to
-       system_stm32l1xx.c file
+  /*!< At this stage the microcontroller clock setting is already configured,
+      this is done through SystemInit() function which is called from startup
+      file (startup_stm32l1xx_xx.s) before to branch to application main.
+      To reconfigure the default setting of SystemInit() function, refer to
+      system_stm32l1xx.c file
      */
 
   /* GPIOD Periph clock enable */
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOD, ENABLE);
 
   /* Configure PD0 and PD1 or PD3 and PD7 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_PIN_X | GPIO_PIN_Y;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-  /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory. 
-     You can monitor PD0 and  PD1 or PD3 and PD7 on the scope to measure the output signal. 
-     If you need to fine tune this frequency, you can add more GPIO set/reset 
+  /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
+     You can monitor PD0 and  PD1 or PD3 and PD7 on the scope to measure the output signal.
+     If you need to fine tune this frequency, you can add more GPIO set/reset
      cycles to minimize more the infinite loop timing.
      This code needs to be compiled with high speed optimization option.  */
   while (1)
@@ -163,7 +159,7 @@ void Delay(__IO uint32_t nCount)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
@@ -176,10 +172,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-  */ 
+  */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
