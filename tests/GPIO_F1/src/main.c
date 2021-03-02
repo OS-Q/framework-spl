78a87ec430ee78dfe34c79fb4af4f54d048f6a02
qitas <qitas@qitas.cn>
2021-03-02 17:27:24 +0800

    fix

--- tests/GPIO_F1/src/main.c
+++ tests/GPIO_F1/src/main.c
@@ -21,7 +21,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
-#include "stm32f10x_gpio.h"
+// #include "stm32f10x_gpio.h"
 
 /** @addtogroup STM32F10x_StdPeriph_Examples
   * @{
@@ -56,13 +56,16 @@ int main(void)
 
   /* GPIOD Periph clock enable */
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  /* Enable GPIOA, GPIOB, RCC_APB2Periph_GPIO_KEY_BUTTON and AFIO clocks */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+                        RCC_APB2Periph_AFIO, ENABLE);
 
   /* Configure PD0 and PD2 in output pushpull mode */
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
-
+  GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
   /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
      You can monitor PD0 or PD2 on the scope to measure the output signal.
      If you need to fine tune this frequency, you can add more GPIO set/reset
